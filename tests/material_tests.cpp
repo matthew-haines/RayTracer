31c8cb97ef26bb862dcf3bd049ad0b789191c599
matthew-haines <m.haines1000@gmail.com>
2021-01-07 14:35:11 -0500

    fixed test case linking and structure

--- tests/material_tests.cpp
+++ tests/material_tests.cpp
@@ -1,22 +1,21 @@
-#include "specular_reflect_brdf.hpp"
-#include <istream>
-#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "fresnel.hpp"
-#include "specular_reflect_brdf.cpp"
+#include "specular_reflect_brdf.hpp"
+#include <iomanip>
 #include <iostream>
 #include <string>
 
-// TODO: Fix this: https://github.com/catchorg/Catch2/blob/devel/docs/slow-compiles.md#top
-
+using namespace std;
 TEST_CASE("FresnelCorrectnessTest", "[.fresnel]") {
     double refractionIndex = 1.5;
     int n = 10;
-    using namespace std;
-    cout << left << setw(12) << " theta" << left << setw(12) << " cos(theta)" << left << setw(12) << " f(theta)" << std::endl;
+    std::cout << left << setw(12) << " theta" << left << setw(12) << 
+        " cos(theta)" << left << setw(12) << " f(theta)" << std::endl;
     for (int i = 0; i < n; i++) {
         double theta = M_PI - M_PI * ((double)i / n);
-        cout << right << setw(12) << to_string(theta) + "," << right << setw(12) << to_string(cos(theta)) + "," << right << setw(12) << FresnelDielectric(cos(theta), refractionIndex, 1) << endl;
+        std::cout << right << setw(12) << to_string(theta) + "," << right << 
+            setw(12) << to_string(cos(theta)) + "," << right << setw(12) << 
+            FresnelDielectric(cos(theta), refractionIndex, 1) << std::endl;
    }
 }
 
